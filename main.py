c25746697fb15736df619d04529b92c1a8d0b726
Dresden6 <dresdenfriar@icloud.com>
2025-03-01 12:08:27 -0700

    Add random background

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+pygame.display.set_caption('Biology Platformer')
+
+
+# create a surface object, image is drawn on it.
+imp = pygame.image.load("./assets/testBackground.jpg").convert()
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -14,9 +20,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+
+
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
+
+    screen.blit(imp, (0, 0))
+    
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
