52141dfeb66ae4711451b8f865039f047ee6f48d
Dresden6 <dresdenfriar@icloud.com>
2025-03-01 17:16:15 -0700

    Make door class

---
+++ door.py
@@ -0,0 +1,62 @@
+import pygame
+from random import randint
+
+class Virus(pygame.sprite.Sprite):
+    # creates the virus
+    def __init__(self, width, height, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.maxspeed = 3
+        
+        self.width = width
+        self.height = height
+        
+        self.radius = width / 4
+
+        self.attacking = False
+        
+        self.x = x
+        self.y = y
+
+        self.image = pygame.image.load("./assets/virus/virus_idle.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * 2, self.image.get_height() * 2))
+        
+        self.rect = self.image.get_rect()
+        self.xspeed = randint(-self.maxspeed, self.maxspeed)
+        self.yspeed = randint(-self.maxspeed, self.maxspeed)
+        self.hit_countdown = 0
+    
+    # trying to make the virus flash when it is hit
+    def update(self):      
+        self._move()
+
+        self.hit_countdown += 1
+
+        if (self.hit_countdown >= 100 ):
+            self.hit_countdown = 0
+            if (self.attacking):
+                self.idle()
+            else:
+                self.attack()
+
+    # makes the virus move randomly back and forth across the screen
+    def _move(self):
+        self.x += self.xspeed
+        self.y += self.yspeed
+        # if not self.rect.contains(newpos):
+        #     self.speed = -self.speed
+        #     newpos = self.rect.move((self.speed, 0))
+        #     self.image = pygame.transform.flip(self.image, True, False)
+    
+    
+    def attack(self):
+        self.image = pygame.image.load("./assets/virus/virus_attack.png").convert_alpha()
+        self.attacking = True
+    
+    def idle(self):
+        self.image = pygame.image.load("./assets/virus/virus_idle.png").convert_alpha()
+        self.attacking = False
+    
+    def getLocation(self):
+        return (self.rect.x, self.rect.y)
+
